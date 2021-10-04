c59dc312fbedb99fd1224a2aaaa8920be6be3d62
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-10-03 22:30:23 -0400

    added localStorage/history functionality

--- script.js
+++ script.js
@@ -99,9 +99,42 @@ for (i=0; i<forecastEls.length; i++) {
 // When the user clicks on a city in the search history
 // Then they are again presented with current and future conditions for that city
 
-
-//  When search button is clicked, read the city name typed by the user
-
-
-//  Using saved city name, execute a 5-day forecast get request from open weather map api
-
+searchEl.addEventListener("click",function() {
+    const searchTerm = inputEl.value;
+    getWeather(searchTerm);
+    searchHistory.push(searchTerm);
+    localStorage.setItem("search",JSON.stringify(searchHistory));
+    renderSearchHistory();
+    })
+    
+    clearEl.addEventListener("click",function() {
+    searchHistory = [];
+    renderSearchHistory();
+    })
+    
+    function k2f(K) {
+    return Math.floor((K - 273.15) *1.8 +32);
+    }
+    
+    function renderSearchHistory() {
+    historyEl.innerHTML = "";
+    for (let i=0; i<searchHistory.length; i++) {
+    const historyItem = document.createElement("input");
+    // <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"></input>
+    historyItem.setAttribute("type","text");
+    historyItem.setAttribute("readonly",true);
+    historyItem.setAttribute("class", "form-control d-block bg-white");
+    historyItem.setAttribute("value", searchHistory[i]);
+    historyItem.addEventListener("click",function() {
+        getWeather(historyItem.value);
+    })
+    historyEl.append(historyItem);
+    }
+    }
+    
+    renderSearchHistory();
+    if (searchHistory.length > 0) {
+    getWeather(searchHistory[searchHistory.length - 1]);
+    }}
+    
+    initPage();
