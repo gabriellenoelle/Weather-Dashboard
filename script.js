d831a37599ecd94df1a30c8752d93f2ad64dd4bb
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-10-03 22:26:42 -0400

    created getweatherfunction

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");4
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+    
+
+    const APIKey = "c9a9ed03a355403f4cb9a36e931c0b4a";
+
 // When a city is searched for
 // Then the user is presented with current and future conditions for that city
 // And the city is added to the search history
+
+function getWeather(cityName) {
+    //  Using saved city name, execute a current condition get request from open weather map api
+            let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+            axios.get(queryURL)
+            .then(function(response){
+                
+                console.log(response);
 
 // When the user views current weather conditions for that city
 // Then they are presented with the city name, the date, a representative icon of weather conditions,
@@ -15,3 +40,8 @@
 
 // When the user clicks on a city in the search history
 // Then they are again presented with current and future conditions for that city
+
+
+//  When search button is clicked, read the city name typed by the user
+
+ 
