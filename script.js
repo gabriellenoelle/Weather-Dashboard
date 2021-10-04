1a2bacbab31a3a984d8168f0434063e3eec3f1e3
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-10-03 22:16:34 -0400

    initial files and added pseudocode to js

---
+++ script.js
@@ -0,0 +1,17 @@
+// When a city is searched for
+// Then the user is presented with current and future conditions for that city
+// And the city is added to the search history
+
+// When the user views current weather conditions for that city
+// Then they are presented with the city name, the date, a representative icon of weather conditions,
+// ... the temperature, the humidity, the wind speed, and the UV index
+
+// When the user views the UV index
+// Then they are presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+// When the user views the future weather conditions for that city
+// Then they are presented with a 5-day forecast that displays the date, a repesentative icon of weather conditions,
+// ... the temperature, the wind speed, and the humidity
+
+// When the user clicks on a city in the search history
+// Then they are again presented with current and future conditions for that city
